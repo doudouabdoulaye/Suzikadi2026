f25f13d928612ffa4dfeb7cd54bab887cad77c31
doudouabdoulaye <abdoulayedoudou99@gmail.com>
2026-04-09 16:38:26 +0100

    fix: publicDir pour copier _redirects dans dist

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,12 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss()],
+   publicDir: 'public',
   server: {
     watch: {
       usePolling: true,
       interval: 100, // Vérifie les changements toutes les 100ms
     },
+
   },
 })
